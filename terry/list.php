560d3c4608c8c58a09ca7b8604336c51f2df4ecb
Andrew <siebe41@gmail.com>
2014-10-23 19:03:37 -0500

    initial upload
    uploading files Andrew and Terry created as tests.

---
+++ terry/list.php
@@ -0,0 +1,30 @@
+<?php 
+
+$link = mysql_connect('phoenixwcus.ipagemysql.com', 'softeng', 'buildmeaprototype'); 
+
+if (!$link) { 
+    die('Could not connect: ' . mysql_error()); 
+} 
+echo 'Connected successfully'; 
+mysql_select_db(cs_soft_eng); 
+
+$q = "SELECT * FROM `class_list` WHERE `number` < 70050";
+
+$res = mysql_query($q,$link);
+$count = mysql_num_rows($res);
+echo '<br>';
+while($count > 0){
+	$count--;
+	$row = mysql_fetch_row($res);
+	echo $row[0].' '.$row[1].' '.$row[2].' '.$row[3].' '.$row[0].'<br>';
+
+}
+
+?> 
+
+<html>
+<body>
+Hello
+</body>
+</html>
+
